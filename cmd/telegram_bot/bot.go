9a23da72bba1f7313d118407dc85896caf452378
esuwu <sanya554455@gmail.com>
2022-02-10 09:03:35 +0300

    Addeed a stable version of webhook client

--- cmd/telegram_bot/bot.go
+++ cmd/telegram_bot/bot.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/asaskevich/govalidator"
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 	"go.uber.org/zap"
-	tb "gopkg.in/tucnak/telebot.v2"
+	tele "gopkg.in/telebot.v3"
 	"nodemon/pkg/bots/telegram/config"
 	"os"
 )
 
-func init() {
-	err := godotenv.Load()
-
+const(
+	botToken = "5054144081:AAFeGslb-lhF3ujPAA2Ogtn1U5DbcC1U36U"
+)
+//func init() {
+func in() {
+	//err := godotenv.Load()
 	if err := config.InitLog(); err != nil {
 		panic(err)
 	}
 
-	if err != nil {
-		zap.S().Infof("No .env file found: %v", err)
-	}
+	//if err != nil {
+	//	zap.S().Infof("No .env file found: %v", err)
+	//}
 	govalidator.SetFieldsRequiredByDefault(true)
 }
 
@@ -36,27 +40,40 @@ func main() {
 }
 
 func run() error {
-	appConfig, err := config.LoadAppConfig()
-	if err != nil {
-		return config.InvalidParameters
+	//appConfig, err := config.LoadAppConfig()
+	//if err != nil {
+	//	return config.InvalidParameters
+	//}
+	in()
+	webhook := &tele.Webhook{
+		Listen:   ":8081",
+		Endpoint: &tele.WebhookEndpoint{PublicURL: "https://mainnet-go-htz-fsn1-1.wavesnodes.com/bot"}, // , Cert: "webhook_cert.pem"
+		//TLS:&tb.WebhookTLS{Key: "webhook_pkey.pem", Cert: "webhook_cert.pem"},
 	}
 
-	webhook := &tb.Webhook{
-		Listen:   ":2000",
-		Endpoint: &tb.WebhookEndpoint{PublicURL: appConfig.WebHookAddress()},
-	}
-
-	botSettings := tb.Settings{
-		Token:  appConfig.BotToken(),
+	botSettings := tele.Settings{
+		Token:  botToken,
 		Poller: webhook,
 	}
 
-	bot, err := tb.NewBot(botSettings)
+	bot, err := tele.NewBot(botSettings)
 	if err != nil {
 		zap.S().Fatalf("failed to start bot, %v", err)
 	}
 
+	bot.Handle("/hello", func(c tele.Context) error {
+		return c.Send("Hello!")
+	})
+
+	/**/
+
+	/**/
+	fmt.Println(bot.URL)
+	fmt.Print(bot.Token)
 	bot.Start()
+
 	zap.S().Info("finished")
 	return nil
 }
+
+//curl -F "url=https://mainnet-go-htz-fsn1-1.wavesnodes.com/bot" https://api.telegram.org/bot5054144081:AAFeGslb-lhF3ujPAA2Ogtn1U5DbcC1U36U/setWebhookq
