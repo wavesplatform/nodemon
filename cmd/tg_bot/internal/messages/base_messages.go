2ef68dfb3c13906adfcd1be6ca1ba1b27895b945
esuwu <sanya554455@gmail.com>
2022-04-25 10:38:42 +0300

    Updated messages

--- cmd/tg_bot/internal/messages/base_messages.go
+++ cmd/tg_bot/internal/messages/base_messages.go
@@ -6,7 +6,7 @@ const (
 	PongMsg       = "🏓"
 
 	HelpInfoText = infoMsg + " This is a bot for monitoring Waves nodes. The next commands are available:\n\n" +
-		"/ping -  the command to check whether the bot is available\n" +
+		"/ping -  the command to check whether the bot is available and what his current state is\n" +
 		"/hello - the command to make the bot <b>save this chat for alerts</b>. Needs to be done first time\n" +
 		"/start - the command to make the bot <b>start getting alerts</b>\n" +
 		"/mute -  the command to make the bot <b>stop listening to alerts</b>" +
