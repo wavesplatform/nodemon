39dc12183dc47efeeb61f8683ca822530532c56f
Nikolay Eskov <mr.eskov1@yandex.ru>
2022-08-17 16:22:35 +0300

    Refactor 'version' method of 'nodeClient'.
    'github.com/wavesplatform/gowaves' dependency updated.

--- pkg/scraping/client.go
+++ pkg/scraping/client.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"net/url"
-	"path"
 	"time"
 
 	"github.com/wavesplatform/gowaves/pkg/client"
@@ -28,28 +26,13 @@ func newNodeClient(url string, timeout time.Duration) *nodeClient {
 }
 
 func (c *nodeClient) version(ctx context.Context) (string, error) {
-	type versionResponse struct {
-		Version string `json:"version"`
-	}
-	nodeURL := c.cl.GetOptions().BaseUrl
-	nodeVersionUrl, err := joinPath(nodeURL, "/node/version")
+	version, _, err := c.cl.NodeInfo.Version(ctx)
 	if err != nil {
-		log.Printf("Failed to join path to the node url while getting version, %v", err)
-		return "", err
-	}
-	versionRequest, err := http.NewRequest("GET", nodeVersionUrl.String(), nil)
-	if err != nil {
-		log.Printf("Creation of version request to %q failed: %v", nodeURL, err)
-		return "", err
-	}
-	versionRequest.Close = true
-	resp := new(versionResponse)
-	_, err = c.cl.Do(ctx, versionRequest, resp)
-	if err != nil {
+		nodeURL := c.cl.GetOptions().BaseUrl
 		log.Printf("Version request to %q failed: %v", nodeURL, err)
 		return "", err
 	}
-	return resp.Version, nil
+	return version, nil
 }
 
 func (c *nodeClient) height(ctx context.Context) (int, error) {
@@ -70,18 +53,4 @@ func (c *nodeClient) stateHash(ctx context.Context, height int) (*proto.StateHas
 		return nil, err
 	}
 	return sh, nil
-}
-
-func joinPath(baseRaw, pathRow string) (*url.URL, error) {
-	baseUrl, err := url.Parse(baseRaw)
-	if err != nil {
-		return nil, err
-	}
-	addUrl, err := url.Parse(pathRow)
-	if err != nil {
-		return nil, err
-	}
-	baseUrl.Path = path.Join(baseUrl.Path, addUrl.Path)
-
-	return baseUrl, nil
 }
