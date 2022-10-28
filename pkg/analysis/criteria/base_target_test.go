9c72fdde0ad7d235e50084cf6bdbd30ca49b1e9f
esuwu <sanya554455@gmail.com>
2022-10-28 00:22:41 -0500

    Added a negative test case

--- pkg/analysis/criteria/base_target_test.go
+++ pkg/analysis/criteria/base_target_test.go
@@ -1,7 +1,6 @@
 package criteria
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -30,56 +29,98 @@ func mkBaseTargetStatements(baseTargetInfo []baseTargetInfo) entities.NodeStatem
 
 func TestBaseTargetCriterion_Analyze(t *testing.T) {
 	var timestamp int64 = 1
-	tests := []struct {
+	test := struct {
 		opts           *BaseTargetCriterionOptions
 		data           entities.NodeStatements
 		expectedAlerts []entities.BaseTargetAlert
 	}{
-		{
-			opts: &BaseTargetCriterionOptions{Threshold: 20},
-			data: mkBaseTargetStatements([]baseTargetInfo{
-				{node: "node1", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 21},
-				{node: "node2", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 16},
-				{node: "node3", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 21},
-				{node: "node4", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 13},
-			}),
-			expectedAlerts: []entities.BaseTargetAlert{
-				{
-					Timestamp:        1,
-					BaseTargetValues: []entities.BaseTargetValue{{Node: "node1", BaseTarget: 21}, {Node: "node2", BaseTarget: 16}, {Node: "node3", BaseTarget: 21}, {Node: "node4", BaseTarget: 13}},
-					Threshold:        20,
-				},
+		opts: &BaseTargetCriterionOptions{Threshold: 20},
+		data: mkBaseTargetStatements([]baseTargetInfo{
+			{node: "node1", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 21},
+			{node: "node2", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 16},
+			{node: "node3", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 21},
+			{node: "node4", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 13},
+		}),
+		expectedAlerts: []entities.BaseTargetAlert{
+			{
+				Timestamp:        timestamp,
+				BaseTargetValues: []entities.BaseTargetValue{{Node: "node1", BaseTarget: 21}, {Node: "node2", BaseTarget: 16}, {Node: "node3", BaseTarget: 21}, {Node: "node4", BaseTarget: 13}},
+				Threshold:        20,
 			},
 		},
 	}
-	for i := range tests {
-		test := tests[i]
-		t.Run(fmt.Sprintf("TestCase#%d", i+1), func(t *testing.T) {
-			alerts := make(chan entities.Alert)
-			done := make(chan struct{})
-			defer func() {
-				select {
-				case <-done:
-					// ok
-				case <-time.After(5 * time.Second):
-					require.Fail(t, "timeout exceeded")
-				}
-			}()
-			go func() {
-				defer close(done)
-				criterion, err := NewBaseTargetCriterion(test.opts)
-				require.NoError(t, err)
-				criterion.Analyze(alerts, timestamp, test.data)
-			}()
-			for j := range test.expectedAlerts {
-				select {
-				case actualAlert := <-alerts:
-					baseTargetAlert := *actualAlert.(*entities.BaseTargetAlert)
-					require.Contains(t, test.expectedAlerts, baseTargetAlert, "test case #%d", j+1)
-				case <-time.After(5 * time.Second):
-					require.Fail(t, "timeout exceeded")
-				}
+
+	t.Run("TestCase Base Target Alert", func(t *testing.T) {
+		alerts := make(chan entities.Alert)
+		done := make(chan struct{})
+		defer func() {
+			select {
+			case <-done:
+				// ok
+			case <-time.After(5 * time.Second):
+				require.Fail(t, "timeout exceeded")
 			}
-		})
+		}()
+		go func() {
+			defer close(done)
+			criterion, err := NewBaseTargetCriterion(test.opts)
+			require.NoError(t, err)
+			criterion.Analyze(alerts, timestamp, test.data)
+		}()
+		for j := range test.expectedAlerts {
+			select {
+			case actualAlert := <-alerts:
+				baseTargetAlert := *actualAlert.(*entities.BaseTargetAlert)
+				require.Contains(t, test.expectedAlerts, baseTargetAlert, "test case #%d", j+1)
+			case <-time.After(5 * time.Second):
+				require.Fail(t, "timeout exceeded")
+			}
+		}
+	})
+}
+
+func TestNoBaseTargetAlertCriterion_Analyze(t *testing.T) {
+	var timestamp int64 = 1
+	test := struct {
+		opts           *BaseTargetCriterionOptions
+		data           entities.NodeStatements
+		expectedAlerts []entities.BaseTargetAlert
+	}{
+		opts: &BaseTargetCriterionOptions{Threshold: 20},
+		data: mkBaseTargetStatements([]baseTargetInfo{
+			{node: "node1", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 19},
+			{node: "node2", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 18},
+			{node: "node3", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 19},
+			{node: "node4", ts: timestamp, v: "v1", h: 1, sh: nil, baseTarget: 1},
+		}),
+		expectedAlerts: nil,
 	}
+
+	t.Run("No alert TestCase", func(t *testing.T) {
+		alerts := make(chan entities.Alert)
+		done := make(chan struct{})
+		defer func() {
+			select {
+			case <-done:
+				// ok
+			case <-time.After(5 * time.Second):
+				require.Fail(t, "timeout exceeded")
+			}
+		}()
+		go func() {
+			defer close(done)
+			criterion, err := NewBaseTargetCriterion(test.opts)
+			require.NoError(t, err)
+			criterion.Analyze(alerts, timestamp, test.data)
+		}()
+		for {
+			select {
+			case actualAlert := <-alerts:
+				baseTargetAlert := *actualAlert.(*entities.BaseTargetAlert)
+				require.Fail(t, "unexpected alert: %v", baseTargetAlert)
+			case <-time.After(5 * time.Second):
+				return
+			}
+		}
+	})
 }
