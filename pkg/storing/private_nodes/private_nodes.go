f93bf53b80877fb330dbb5931a82f0f78455c5d3
Nikolay Eskov <mr.eskov1@yandex.ru>
2022-11-16 05:20:37 +0300

    Add 'PrivateNodesHandler.handleEventWithTs' method for processing single event.

--- pkg/storing/private_nodes/private_nodes.go
+++ pkg/storing/private_nodes/private_nodes.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"nodemon/pkg/entities"
 	"nodemon/pkg/storing/events"
@@ -55,25 +56,8 @@ func (h *PrivateNodesHandler) putPrivateNodesEvents(ts int64) entities.Nodes {
 	h.privateEvents.Mu.RLock()
 	defer h.privateEvents.Mu.RUnlock()
 	for node, event := range h.privateEvents.Data {
-		switch e := event.(type) {
-		case *entities.InvalidHeightEvent:
-			updatedEvent := entities.NewInvalidHeightEvent(e.Node(), ts, e.Version(), e.Height())
-			err := h.es.PutEvent(updatedEvent)
-			if err != nil {
-				h.zap.Error("Failed to put event", zap.Error(err))
-				return nodes
-			}
-			h.zap.Sugar().Infof("Statement 'InvalidHeight' for private node %s has been put into the storage, height %d", node, e.Height())
-		case *entities.StateHashEvent:
-			updatedEvent := entities.NewStateHashEvent(e.Node(), ts, e.Version(), e.Height(), e.StateHash(), e.BaseTarget())
-			err := h.es.PutEvent(updatedEvent)
-			if err != nil {
-				h.zap.Error("Failed to put event", zap.Error(err))
-				return nodes
-			}
-			h.zap.Sugar().Infof("Statement for private node %s has been put into the storage, height %d, statehash %s", node, e.Height(), e.StateHash().SumHash.Hex())
-		default:
-			h.zap.Sugar().Errorf("Unknown event type (%T)", e)
+		if err := h.handleEventWithTs(ts, event); err != nil {
+			h.zap.Error("Failed to put private node event", zap.Error(err))
 			return nodes
 		}
 		nodes = append(nodes, node)
@@ -81,13 +65,41 @@ func (h *PrivateNodesHandler) putPrivateNodesEvents(ts int64) entities.Nodes {
 	return nodes
 }
 
+func (h *PrivateNodesHandler) handleEventWithTs(ts int64, e entities.Event) error {
+	switch e := e.(type) {
+	case *entities.InvalidHeightEvent:
+		updatedEvent := entities.NewInvalidHeightEvent(e.Node(), ts, e.Version(), e.Height())
+		err := h.es.PutEvent(updatedEvent)
+		if err != nil {
+			return errors.Wrapf(err, "failed to put event (%T) for node %s", e, e.Node())
+		}
+		h.zap.Sugar().Infof("Statement (%T) for private node %s has been put into the storage, height %d", e, e.Node(), e.Height())
+	case *entities.StateHashEvent:
+		updatedEvent := entities.NewStateHashEvent(e.Node(), ts, e.Version(), e.Height(), e.StateHash(), e.BaseTarget())
+		err := h.es.PutEvent(updatedEvent)
+		if err != nil {
+			return errors.Wrapf(err, "failed to put event (%T) for node %s", e, e.Node())
+		}
+		h.zap.Sugar().Infof("Statement (%T) for private node %s has been put into the storage, height %d, statehash %s",
+			e, e.Node(), e.Height(), e.StateHash().SumHash.Hex(),
+		)
+	default:
+		return errors.Errorf("unknown event type (%T) for node %s", e, e.Node())
+	}
+	return nil
+}
+
 func (h *PrivateNodesHandler) HandlePrivateEvents(wrappedNotifications <-chan entities.WrappedNotification, notifications chan<- entities.Notification) {
 	for wn := range wrappedNotifications {
 		switch notification := wn.(type) {
 		case *entities.OnPollingComplete:
-			polledNodes := notification.Nodes()
-			storedPrivateNodes := h.putPrivateNodesEvents(notification.Ts)
-			notifications <- entities.NewOnPollingComplete(append(polledNodes, storedPrivateNodes...), notification.Ts)
+			var (
+				ts          = notification.Ts
+				polledNodes = notification.Nodes()
+			)
+			storedPrivateNodes := h.putPrivateNodesEvents(ts)
+			h.zap.Sugar().Infof("Total count of stored private nodes statements is %d at timestamp %d", len(storedPrivateNodes), ts)
+			notifications <- entities.NewOnPollingComplete(append(polledNodes, storedPrivateNodes...), ts)
 		default:
 			h.zap.Error("unknown analyzer notification", zap.String("type", fmt.Sprintf("%T", notification)))
 		}
