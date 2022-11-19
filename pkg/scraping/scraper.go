3e869e3b2bf5553c84795b6a73d29566ee668c4d
Nikolay Eskov <mr.eskov1@yandex.ru>
2022-11-19 16:09:55 +0300

    Fixed deadlock in 'Scraper.poll'. (#126)

--- pkg/scraping/scraper.go
+++ pkg/scraping/scraper.go
@@ -49,78 +49,84 @@ func (s *Scraper) Start(ctx context.Context) (<-chan entities.Notification, *ato
 }
 
 func (s *Scraper) poll(ctx context.Context, notifications chan<- entities.Notification, now int64) {
-	cc, cancel := context.WithCancel(ctx)
+	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-
-	var wg sync.WaitGroup
-	ec := make(chan entities.Event)
-	defer close(ec)
 
 	enabledNodes, err := s.ns.EnabledNodes()
 	if err != nil {
 		s.zap.Error("Failed to get nodes from storage", zap.Error(err))
 	}
-	wg.Add(len(enabledNodes))
-	for i := range enabledNodes {
-		n := enabledNodes[i]
-		go func() {
-			s.queryNode(cc, n.URL, ec, now)
-		}()
-	}
 
-	go func() {
-		cnt := 0
-		for e := range ec {
-			if err := s.es.PutEvent(e); err != nil {
-				s.zap.Sugar().Errorf("Failed to collect event '%T' from node %s: %v", e, e.Node(), err)
-			}
-			switch e.(type) {
-			case *entities.UnreachableEvent, *entities.InvalidHeightEvent, *entities.StateHashEvent:
-				wg.Done()
-			}
+	ec := s.queryNodes(ctx, enabledNodes, now)
+	cnt := 0
+	for e := range ec {
+		if err := s.es.PutEvent(e); err != nil {
+			s.zap.Sugar().Errorf("Failed to collect event '%T' from node %s, statement=%+v: %v", e, e.Node(), e.Statement(), err)
+		} else {
 			cnt++
 		}
-		s.zap.Sugar().Infof("Polling of %d nodes completed with %d events collected", len(enabledNodes), cnt)
-	}()
-	wg.Wait()
+	}
+	s.zap.Sugar().Infof("Polling of %d nodes completed with %d events saved", len(enabledNodes), cnt)
+
 	urls := make([]string, len(enabledNodes))
 	for i := range enabledNodes {
 		urls[i] = enabledNodes[i].URL
 	}
-
 	notifications <- entities.NewOnPollingComplete(urls, now)
 }
 
-func (s *Scraper) queryNode(ctx context.Context, url string, events chan entities.Event, ts int64) {
+func (s *Scraper) queryNodes(ctx context.Context, nodes []entities.Node, now int64) <-chan entities.Event {
+	poller := func(ec chan<- entities.Event) {
+		wg := new(sync.WaitGroup)
+		defer func() {
+			wg.Wait()
+			close(ec)
+		}()
+		wg.Add(len(nodes))
+		for i := range nodes {
+			nodeURL := nodes[i].URL
+			go func() {
+				defer wg.Done()
+				event := s.queryNode(ctx, nodeURL, now)
+				s.zap.Sugar().Infof("Collected event (%T) for node %s", event, nodeURL)
+				ec <- event
+			}()
+		}
+	}
+	ec := make(chan entities.Event, len(nodes))
+	go poller(ec)
+	return ec
+}
 
+func (s *Scraper) queryNode(ctx context.Context, url string, ts int64) entities.Event {
 	node := newNodeClient(url, s.timeout, s.zap)
 	v, err := node.version(ctx)
 	if err != nil {
-		events <- entities.NewUnreachableEvent(url, ts)
-		return
+		s.zap.Sugar().Warnf("Failed to get version for node %s: %v", url, err)
+		return entities.NewUnreachableEvent(url, ts)
 	}
-	events <- entities.NewVersionEvent(url, ts, v)
 	h, err := node.height(ctx)
 	if err != nil {
-		return
+		s.zap.Sugar().Warnf("Failed to get height for node %s: %v", url, err)
+		return entities.NewVersionEvent(url, ts, v) // we know version, sending what we know about node
 	}
 	if h < 2 {
-		events <- entities.NewInvalidHeightEvent(url, ts, v, h)
-		return
+		s.zap.Sugar().Warnf("Node %s has invalid height %d", url, h)
+		return entities.NewInvalidHeightEvent(url, ts, v, h)
 	}
-	events <- entities.NewHeightEvent(url, ts, v, h)
 
+	// TODO: request base target for h-1 block
 	bs, err := node.baseTarget(ctx, h)
 	if err != nil {
-		return
+		s.zap.Sugar().Warnf("Failed to get base target for node %s: %v", url, err)
+		return entities.NewHeightEvent(url, ts, v, h) // we know about version and height, sending it
 	}
-	events <- entities.NewBaseTargetEvent(url, ts, v, h, bs)
 
 	h = h - 1 // Go to previous height to request state hash
 	sh, err := node.stateHash(ctx, h)
 	if err != nil {
-		return
+		s.zap.Sugar().Warnf("Failed to get state hash for node %s: %v", url, err)
+		return entities.NewBaseTargetEvent(url, ts, v, h, bs) // we know version, height and base target, sending it
 	}
-	events <- entities.NewStateHashEvent(url, ts, v, h, sh, bs)
-
+	return entities.NewStateHashEvent(url, ts, v, h, sh, bs) // sending full info about node
 }
