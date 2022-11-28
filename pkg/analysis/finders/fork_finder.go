5784a801390bd6aa0c414f05efbad6fdd93a161f
Nikolay Eskov <mr.eskov1@yandex.ru>
2022-11-28 07:35:06 +0300

    Add optimistic linear state hash search for 'ForkFinder'.

--- pkg/analysis/finders/fork_finder.go
+++ pkg/analysis/finders/fork_finder.go
@@ -12,11 +12,25 @@ var (
 )
 
 type ForkFinder struct {
-	storage *events.Storage
+	storage            *events.Storage
+	linearSearchParams *linearSearchParams
+}
+
+type linearSearchParams struct {
+	currentHeight int
+	searchDepth   int
 }
 
 func NewForkFinder(es *events.Storage) *ForkFinder {
 	return &ForkFinder{storage: es}
+}
+
+func (f *ForkFinder) WithLinearSearchParams(currentHeight, searchDepth int) *ForkFinder {
+	f.linearSearchParams = &linearSearchParams{
+		currentHeight: currentHeight,
+		searchDepth:   searchDepth,
+	}
+	return f
 }
 
 func (f *ForkFinder) FindLastCommonBlock(nodeA, nodeB string) (int, proto.BlockID, error) {
@@ -67,6 +81,38 @@ func (f *ForkFinder) FindLastCommonBlock(nodeA, nodeB string) (int, proto.BlockI
 	return r, sh.BlockID, nil
 }
 
+var errNotFound = errors.New("not found")
+
+func (f *ForkFinder) tryLinearStateHashSearch(nodeA, nodeB string, start int) (int, proto.StateHash, error) {
+	if f.linearSearchParams == nil {
+		return 0, proto.StateHash{}, errors.Wrap(errNotFound, "no linear search params provided")
+	}
+	var (
+		depth = f.linearSearchParams.searchDepth
+		currH = f.linearSearchParams.currentHeight
+	)
+	for i := 0; i < depth && currH-start-i > 0; i++ {
+		h := currH - i
+		different, err := f.differentStateHashesAt(nodeA, nodeB, h)
+		if err != nil {
+			if errors.Is(err, events.NoFullStatementError) {
+				continue
+			}
+			return 0, proto.StateHash{}, errors.Wrapf(err,
+				"failed to get statement for nodes '%s' and '%s' at height %d", nodeA, nodeB, h)
+		}
+		if different {
+			continue
+		}
+		sh, err := f.storage.StateHashAtHeight(nodeA, h)
+		if err != nil {
+			return 0, proto.StateHash{}, errors.Wrapf(err, "no statehash at height %d", h)
+		}
+		return h, sh, nil
+	}
+	return 0, proto.StateHash{}, errors.Wrapf(errNotFound, "linear search failed for nodes '%s' and '%s'", nodeA, nodeB)
+}
+
 func (f *ForkFinder) FindLastCommonStateHash(nodeA, nodeB string) (int, proto.StateHash, error) {
 	startA, err := f.storage.EarliestHeight(nodeA)
 	if err != nil {
@@ -77,6 +123,16 @@ func (f *ForkFinder) FindLastCommonStateHash(nodeA, nodeB string) (int, proto.St
 		return 0, proto.StateHash{}, errors.Wrapf(err, "no earliest statement for node '%s'", nodeB)
 	}
 	start := max(startA, startB)
+
+	h, sh, err := f.tryLinearStateHashSearch(nodeA, nodeB, start)
+	switch {
+	case err != nil && !errors.Is(err, errNotFound):
+		err := errors.Wrapf(err, "linear statehash search failed for nodes '%s' and '%s'", nodeA, nodeB)
+		return 0, proto.StateHash{}, err
+	case err == nil:
+		return h, sh, nil
+	}
+
 	stopA, err := f.storage.LatestHeight(nodeA)
 	if err != nil {
 		return 0, proto.StateHash{}, errors.Wrapf(err, "no latest statement for node '%s'", nodeA)
@@ -110,9 +166,9 @@ func (f *ForkFinder) FindLastCommonStateHash(nodeA, nodeB string) (int, proto.St
 			"no common blocks in range [%d, %d]", initialStart, initialStop,
 		)
 	}
-	sh, err := f.storage.StateHashAtHeight(nodeA, r)
+	sh, err = f.storage.StateHashAtHeight(nodeA, r)
 	if err != nil {
-		return 0, proto.StateHash{}, errors.Wrapf(err, "no block ID at height %d", r)
+		return 0, proto.StateHash{}, errors.Wrapf(err, "no statehash ID at height %d", r)
 	}
 	return r, sh, nil
 }
