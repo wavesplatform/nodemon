2162413b35d3822861a91528fd3f012a41805e0d
Nikolay Eskov <mr.eskov1@yandex.ru>
2024-10-17 05:40:35 +0300

    Add '/version' HTTP route to the applications HTTP APIs.

--- internal/version.go
+++ internal/version.go
@@ -1,6 +1,21 @@
 package internal
 
+import (
+	"fmt"
+	"net/http"
+)
+
 var version = "v0.0.0"
 
 // Version returns the version of the application.
 func Version() string { return version }
+
+func VersionHTTPHandler(w http.ResponseWriter, _ *http.Request) {
+	h := w.Header()
+	h.Set("Content-Type", "application/json")
+	h.Set("X-Content-Type-Options", "nosniff")
+	if _, err := fmt.Fprintf(w, "{\"version\":%q}\n", Version()); err != nil {
+		http.Error(w, fmt.Sprintf("{\"error\":%q}\n", err.Error()), http.StatusInternalServerError)
+		return
+	}
+}
