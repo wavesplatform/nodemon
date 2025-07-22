65679af9e894c0385f4c2bb54b4bd13d8ae10eda
Nikolay Eskov <mr.eskov1@yandex.ru>
2025-07-22 20:33:52 +0300

    Use 'slog' in 'scraping' package.

--- pkg/tools/logging/attrs/slog.go
+++ pkg/tools/logging/attrs/slog.go
@@ -1,6 +1,8 @@
 package attrs
 
 import (
+	"encoding"
+	"fmt"
 	"log/slog"
 )
 
@@ -8,4 +10,20 @@ import (
 func Error(err error) slog.Attr {
 	const key = "error"
 	return slog.Any(key, err)
+}
+
+// textMarshaler is a helper function that formats a value as a slog.Attr
+// and checks if the value implements encoding.TextMarshaler.
+func textMarshaler(key string, value encoding.TextMarshaler) slog.Attr { return slog.Any(key, value) }
+
+type typenamePrinter struct{ v any }
+
+func (t typenamePrinter) MarshalText() ([]byte, error) {
+	return fmt.Appendf(nil, "%T", t.v), nil
+}
+
+// Type returns a slog.Attr that contains the type name of the value.
+func Type(value any) slog.Attr {
+	const key = "type"
+	return textMarshaler(key, typenamePrinter{v: value})
 }
