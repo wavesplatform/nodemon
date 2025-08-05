6cc4f7a09258c857b9a54d26e7eaef6cd13f7014
Nikolay Eskov <mr.eskov1@yandex.ru>
2025-08-05 21:52:30 +0300

    Used 'Error' and 'Type' slog helper functions from 'github.com/wavesplatform/gowaves/pkg/logging'.

--- pkg/tools/logging/attrs/slog.go
+++ pkg/tools/logging/attrs/slog.go
@@ -7,28 +7,22 @@ import (
 	"log/slog"
 	"strings"
 	"unicode/utf8"
+
+	gl "github.com/wavesplatform/gowaves/pkg/logging"
 )
 
 // Error returns a slog.Attr that contains the error value with the key "error".
 func Error(err error) slog.Attr {
-	const key = "error"
-	return slog.Any(key, err)
+	return gl.Error(err)
 }
 
 // textMarshaler is a helper function that formats a value as a slog.Attr
 // and checks if the value implements encoding.TextMarshaler.
 func textMarshaler(key string, value encoding.TextMarshaler) slog.Attr { return slog.Any(key, value) }
 
-type typenamePrinter struct{ v any }
-
-func (t typenamePrinter) MarshalText() ([]byte, error) {
-	return fmt.Appendf(nil, "%T", t.v), nil
-}
-
 // Type returns a slog.Attr that contains the type name of the value.
 func Type(value any) slog.Attr {
-	const key = "type"
-	return textMarshaler(key, typenamePrinter{v: value})
+	return gl.Type(value)
 }
 
 type byteStringPrinter []byte
